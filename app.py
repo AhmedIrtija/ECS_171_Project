5a18ec1f42e3e1e9ade974ee1aa59777e2ec685a
AhmedIrtija <airtija@ucdavis.edu>
2023-11-29 15:26:21 -0800

    Web app fixed

--- app.py
+++ app.py
@@ -6,23 +6,19 @@ from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
 
-# Load the trained model, vectorizer, and multi-label binarizer
+# Load the best trained model, vectorizer, and multi-label binarizer
 trained_model = load_model('best_model.h5')
 vectorizer = CountVectorizer()
 mlb = MultiLabelBinarizer()
 
-# Assuming 'Allergen_Status_of_Food_Products.csv' contains the same columns as in the training script
 data = pd.read_csv("Allergen_Status_of_Food_Products.csv")
 
-# Fill NaN values with empty strings in the relevant columns
 data = data.fillna('')
 
-# Transform the input features using the vectorizer
 X = vectorizer.fit_transform(data['Food Product'] + " " + data['Main Ingredient'] + " " +
                              data['Sweetener'] + " " + data['Fat/Oil'] + " " + data['Seasoning'])
 y = mlb.fit_transform(data['Allergens'].str.split(', '))
 
-# Homepage
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -44,16 +40,22 @@ def predict():
     fat_oil = '' if pd.isna(fat_oil) else fat_oil
     seasoning = '' if pd.isna(seasoning) else seasoning
 
+
+
     # Transform the input using the vectorizer
     input_data = vectorizer.transform([f"{food_product} {main_ingredient} {sweetener} {fat_oil} {seasoning}"])
     input_data_dense = input_data.toarray()
 
+
     # Get model predictions
     predictions = trained_model.predict(input_data_dense)
-    allergen_probabilities = {allergen: prob for allergen, prob in zip(mlb.classes_, predictions[0])}
+
+    allergen_probabilities = {allergen: f"{prob:.2f}%" for allergen, prob in zip(mlb.classes_, 100 * predictions[0])}
+
+    filtered_allergens = {allergen: prob for allergen, prob in allergen_probabilities.items() if float(prob[:-1]) >= 80}
 
     # Display the result on result.html
-    return render_template('result.html', allergen_probabilities=allergen_probabilities)
+    return render_template('result.html', allergen_probabilities=filtered_allergens)
 
 if __name__ == '__main__':
     app.run(debug=True)
